c77cf90b456c7af3f20e3c8c573ef9e67b76ccfc
Rahul Baphana <rahul.kbaphana@yahoo.com>
2021-01-31 22:07:35 +0000

    Add data integrity violation test

--- src/test/java/com/participant/registry/domain/participant/repository/ParticipantRepositoryTest.java
+++ src/test/java/com/participant/registry/domain/participant/repository/ParticipantRepositoryTest.java
@@ -4,9 +4,9 @@ import com.participant.registry.BaseIntegrationTest;
 import com.participant.registry.domain.participant.entity.Participant;
 import com.participant.registry.support.DateUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.dao.DataIntegrityViolationException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParticipantRepositoryTest extends BaseIntegrationTest {
     @Test
@@ -31,5 +31,35 @@ public class ParticipantRepositoryTest extends BaseIntegrationTest {
         assertEquals("John Doe", savedJohnDoeRecord.getName());
         assertEquals("0778956655", savedJohnDoeRecord.getPhoneNumber());
         assertEquals("25-09-1987", DateUtil.stringForDateOfBirthFormat(savedJohnDoeRecord.getDateOfBirth()));
+    }
+
+    @Test
+    void should_throw_exception_while_saving_two_participants_with_same_reference_number() {
+        // Given
+        String referenceNumberForJohnDoe = "KFG-123";
+        Participant johnDoe = Participant.builder()
+            .referenceNumber(referenceNumberForJohnDoe)
+            .name("John Doe")
+            .dateOfBirth(DateUtil.dateOfBirthFormat("25-09-1987"))
+            .address("AddressLine 1, USA - PostCode")
+            .phoneNumber("0778956655")
+            .build();
+
+        // When
+        participantRepository.save(johnDoe);
+
+        // Then
+        assertThrows(DataIntegrityViolationException.class,
+            () -> participantRepository.save(participantWithReference(referenceNumberForJohnDoe)));
+    }
+
+    private Participant participantWithReference(String referenceNumber) {
+        return Participant.builder()
+            .name("Jane Doe")
+            .address("Some address")
+            .phoneNumber("1234567891")
+            .dateOfBirth(DateUtil.dateOfBirthFormat("01-11-1988"))
+            .referenceNumber(referenceNumber)
+            .build();
     }
 }
